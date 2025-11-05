16d0f4fb0527afed956e908ef5840e3a79033bb9
ronaldmark.antonio <ronaldmark.antonio@gmail.com>
2025-11-05 22:00:27 +0800

    Update passport.js

--- passport.js
+++ passport.js
@@ -7,8 +7,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || "/users/google/callback",
-      passReqToCallback: true
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
       return done(null, profile);
